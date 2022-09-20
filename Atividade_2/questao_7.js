8a0aaabe01a36103702e5dfbfe4a64c1c371ca07
Joaab Sousa <48816108+joaabjb@users.noreply.github.com>
2022-09-20 02:24:55 -0300

    Add files via upload

---
+++ Atividade_2/questao_7.js
@@ -0,0 +1,5 @@
+var nome = 'Ely';
+var time = 120.56;
+var lang = 'Typescript';
+var frase = "".concat(nome, "\nMy payment time is ").concat(time, "\nand\nmy preferred language is ").concat(lang);
+console.log(frase);
---
+++ Atividade_2/questao_7.ts
@@ -0,0 +1,10 @@
+let nome : string = 'Ely';
+let time : number = 120.56;
+let lang : string = 'Typescript';
+
+let frase: string = `${nome}
+My payment time is ${time}
+and
+my preferred language is ${lang}`;
+
+console.log(frase);
